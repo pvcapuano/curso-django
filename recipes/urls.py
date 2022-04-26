04e051fb3dc64fe43f3426dac3a76012718c6b15
pvcapuano <pvcapuano@gmail.com>
2022-04-26 18:45:38 -0300

    module 8 video 7

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from recipes.views import home
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 #
 app_name = 'recipes'
@@ -9,3 +11,6 @@ urlpatterns = [
     path('', home, name="home"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
