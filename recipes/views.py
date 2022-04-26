6947ff50335e7e18c9d81c0b376def2c99eb6689
cpsmetal <103198803+cpsmetal@users.noreply.github.com>
2022-04-26 17:14:11 -0300

    module 7 finished

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'PV',
+         'recipes': [make_recipe() for _ in range(10)],
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
